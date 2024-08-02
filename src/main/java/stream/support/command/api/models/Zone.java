b92e4061795a8b4f62feb275d07629f16683e785
Jonas Sperling <mail@jonas-sperling.de>
2024-08-02 07:46:20 +0200

    Ignore zone id

--- src/main/java/stream/support/command/api/models/Zone.java
+++ src/main/java/stream/support/command/api/models/Zone.java
@@ -1,15 +1,16 @@
 package stream.support.command.api.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.*;
 
 @Setter
 @Getter
 @ToString
+@JsonIgnoreProperties({"id"})
 @NoArgsConstructor
 @AllArgsConstructor
 public class Zone {
     private String name;
     private String flag;
-
     private Zone parent;
 }
